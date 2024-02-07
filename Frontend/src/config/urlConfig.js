7878723d3fffca52d4e3e288c6f3d97e66c106e5
Salma <salma.shaikh751@gmail.com>
2024-02-07 11:12:52 +0100

    modifying main to deploy

--- Frontend/src/config/urlConfig.js
+++
@@ -1,5 +0,0 @@
-let BASE_URL =
-  process.env.NODE_ENV === "development" // implement .env file !!
-    ? "http://localhost:5000"
-    : "https://expensestracker-backend.onrender.com"; //! this is the render url 
-export default BASE_URL;           //! remember to change it!! this is just an example
--- Frontend/src/context/context.jsx
+++
@@ -1,5 +0,0 @@
-import React, { createContext } from "react";
-
-export const context=createContext();
-
-
